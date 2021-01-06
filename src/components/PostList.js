fd42d0935901b35c02252d15fb0637ee230c0e23
kicok <kicoker@gmail.com>
2021-01-06 18:40:58 +0900

    Writing Up an Action Creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
+    console.log(this.props);
     return <div>Post</div>;
   }
 }
 
-export default PostList;
+const mapStateToProps = (state) => {
+  return { dummyReducer: state.dummyReducer };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
