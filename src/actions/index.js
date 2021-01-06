7e0b9ffd3f795371d683e5503e9094a07c9879f8
kicok <kicoker@gmail.com>
2021-01-06 18:50:47 +0900

    Making a Request From an Action Creator - Bad!!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+  // Bad approach!!!!!
+  // Error: Actions must be plain objects. Use custom middleware for async actions.
+  const response = await jsonPlaceholder.get('/posts');
   return {
     type: 'FETCH_POSTS',
+    payload: response,
   };
 };
