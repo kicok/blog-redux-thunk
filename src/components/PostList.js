3803460bad9d096c748dbe99937af8957832b41b
kicok <kicoker@gmail.com>
2021-01-08 14:31:55 +0900

    Dispatching Correct Values

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -13,8 +13,7 @@ class PostList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return { dummyReducer: state.dummyReducer };
+  return { posts: state.posts };
 };
 
 export default connect(mapStateToProps, { fetchPosts })(PostList);
